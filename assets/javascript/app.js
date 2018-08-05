ccbce913861b9255f943c85e14a277cfb6332ca8
RubyG <rubyrgill@hotmail.com>
2018-08-05 16:07:37 -0400

    change background on scroll function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,18 @@
 
 $(function () {
     setTimeout(function () {
-        $(".fly-in-text").removeClass("hidden")
-    }, 500)
-})
+        $(".fly-in-text").removeClass("hidden");
+    }, 500);
+});
+
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $("body").addClass("changeColor")
+            $()
+        }
+        if ($(this).scrollTop() < 50) {
+            $("body").removeClass("changeColor")
+        }
+    });
+});
